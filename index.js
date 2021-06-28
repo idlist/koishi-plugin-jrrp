452525d5ae7c204ec711eae23966a6fc8677a071
i'DLisT <i.dlist@outlook.com>
2021-06-28 20:21:45 +0800

    feat: support luck level

--- index.js
+++ index.js
@@ -1,22 +1,59 @@
 const { createHash } = require('crypto')
+const { t } = require('koishi-core')
 
 class Config {
   constructor(config) {
     config = { ...config }
+
     this.useDatabase = config.useDatabase ?? true
+    this.levels = config.levels ?? true
+    this.levelDescriptions = (this.levels && config.levelDescriptions) ?? config.levelDescriptions
+    this.result = config.result ?? undefined
   }
 }
 
 module.exports.name = 'jrrp'
 
 module.exports.apply = (ctx, config) => {
+  t.set('jrrp.description', '今日人品')
   config = new Config(config)
+
+  let levels = [], desc = {}
+
+  if (config.levels) {
+    t.set('jrrp.result', '{0} 的今日人品是：{1}。{2}')
+
+    if (config.levelDescriptions) {
+      for (const level in config.levelDescriptions) {
+        desc[level] = config.levelDescriptions[level]
+        levels.push(parseInt(level))
+      }
+    } else {
+      desc = {
+        '100': '买彩票可能会中大奖哦！',
+        '80': '出门可能捡到 1 块钱。',
+        '60': '太阳当头照，花儿对你笑。',
+        '40': '还行，还行。',
+        '20': '多扶一扶老奶奶吧。',
+        '0': '推荐闷头睡大觉。'
+      }
+      levels = Object.keys(desc).map(level => parseInt(level))
+    }
+
+    levels = levels.sort((a, b) => a - b)
+  } else {
+    t.set('jrrp.result', '{0} 的今日人品是：{1}')
+  }
+
+  if (config.result) {
+    t.set('jrrp.result', config.result)
+  }
 
   ctx.on('connect', () => {
     if (!ctx.database) config.useDatabase = false
   })
 
-  ctx.command('jrrp', '今日人品')
+  ctx.command('jrrp', t('jrrp.description'))
     .userFields(['name'])
     .action(({ session }) => {
       let name
@@ -29,6 +66,19 @@ module.exports.apply = (ctx, config) => {
       luck.update((new Date().getTime() / (1000 * 60 * 60 * 24)).toFixed(0))
       luck.update('42')
 
-      return name + ' 的今日人品是：' + (parseInt(luck.digest('hex'), 16) % 101)
+      let luckValue = parseInt(luck.digest('hex'), 16) % 101
+
+      if (config.levels) {
+        let descKey = 0
+        for (const level of levels) {
+          if (luckValue >= level) descKey = level
+          else break
+        }
+        let luckText = desc[descKey]
+
+        return t('jrrp.result', name, luckValue, luckText)
+      } else {
+        return t('jrrp.result', name, luckValue)
+      }
     })
 }
